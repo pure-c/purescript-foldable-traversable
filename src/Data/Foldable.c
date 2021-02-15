3e05c556e742e40944b03e130ddd46b953b19339
Felix Schlitter <felixschlitter@gmail.com>
2021-02-16 09:58:07 +1300

    Fix Foldable FFI for real

--- src/Data/Foldable.c
+++ src/Data/Foldable.c
@@ -3,13 +3,13 @@
 PURS_FFI_FUNC_3(Data_Foldable_foldrArray, f, init, _xs) {
 	purs_any_t acc;
 	const purs_vec_t *xs;
-	int acc_changed = 0;
 
 	acc = init;
 	PURS_ANY_RETAIN(acc);
 	xs = purs_any_force_array(_xs);
 
 	if (purs_vec_is_empty(xs)) {
+		PURS_RC_RELEASE(xs);
 		return acc;
 	}
 
@@ -17,9 +17,8 @@ PURS_FFI_FUNC_3(Data_Foldable_foldrArray, f, init, _xs) {
 		purs_any_t tmp1, tmp2;
 		tmp1 = purs_any_app(f, xs->data[i]);
 		tmp2 = purs_any_app(tmp1, acc);
-		if (acc_changed) PURS_ANY_RELEASE(acc);
+		PURS_ANY_RELEASE(acc);
 		acc = tmp2;
-		acc_changed = 1;
 		PURS_ANY_RELEASE(tmp1);
 	}
 
@@ -30,13 +29,13 @@ PURS_FFI_FUNC_3(Data_Foldable_foldrArray, f, init, _xs) {
 PURS_FFI_FUNC_3(Data_Foldable_foldlArray, f, init, _xs) {
 	purs_any_t acc;
 	const purs_vec_t *xs;
-	int acc_changed = 0;
 
 	acc = init;
 	PURS_ANY_RETAIN(acc);
 	xs = purs_any_force_array(_xs);
 
 	if (purs_vec_is_empty(xs)) {
+		PURS_RC_RELEASE(xs);
 		return acc;
 	}
 
@@ -44,9 +43,8 @@ PURS_FFI_FUNC_3(Data_Foldable_foldlArray, f, init, _xs) {
 		purs_any_t tmp1, tmp2;
 		tmp1 = purs_any_app(f, acc);
 		tmp2 = purs_any_app(tmp1, xs->data[i]);
-		if (acc_changed) PURS_ANY_RELEASE(acc);
+		PURS_ANY_RELEASE(acc);
 		acc = tmp2;
-		acc_changed = 1;
 		PURS_ANY_RELEASE(tmp1);
 	}
 
